721722c43ceb05a73bad15058479fa7c8931aacc
shushuai3 <lss8513@gmail.com>
2021-06-04 14:39:55 +0200

    change to 3d relative localization

--- src/modules/interface/relative_localization.h
+++ src/modules/interface/relative_localization.h
@@ -3,12 +3,12 @@
 #include "system.h"
 
 typedef enum {
-  STATE_rlX, STATE_rlY, STATE_rlYaw, STATE_DIM_rl
+  STATE_rlX, STATE_rlY, STATE_rlZ, STATE_rlYaw, STATE_DIM_rl
 } relative_stateIdx_t;
 
 typedef enum
 {
-  INPUT_vxi, INPUT_vyi, INPUT_ri, INPUT_vxj, INPUT_vyj, INPUT_rj, INPUT_DIM
+  INPUT_vxi, INPUT_vyi, INPUT_vzi, INPUT_ri, INPUT_vxj, INPUT_vyj, INPUT_vzj, INPUT_rj, INPUT_DIM
 } relative_inputIdx_t;
 
 typedef struct
@@ -21,6 +21,6 @@ typedef struct
 
 void relativeLocoInit(void);
 void relativeLocoTask(void* arg);
-void relativeEKF(int n, float vxi, float vyi, float ri, float hi, float vxj, float vyj, float rj, float hj, uint16_t dij, float dt);
+void relativeEKF(int n, float vxi, float vyi, float vzi, float ri, float hi, float vxj, float vyj, float vzj, float rj, float hj, uint16_t dij, float dt);
 bool relativeInfoRead(float* relaVarParam, float* inputVarParam);
 #endif
